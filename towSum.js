b4abfba3e1d3e3bdf854c74a57d8fcd808f3b2a3
lqy <413281823@qq.com>
2022-09-05 23:05:48 +0800

    feat: 哈希解决两数之和

---
+++ towSum.js
@@ -0,0 +1,11 @@
+const twoSum = (nums, target) => {
+  const hash = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    // 判断是否存在目标值减去当前值的差的key 因为 target - x = y
+    if (hash.has(target - nums[i])) {
+      return [i, hash.get(target - nums[i])];
+    }
+    // 把当前数组的值做key，下标做值
+    hash.set(nums[i], i);
+  }
+};
