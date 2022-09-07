eb648e74dfe157e0ee3e34005d3bdf637bec804a
lqy <413281823@qq.com>
2022-09-07 22:10:49 +0800

    feat: 两数之和map解法更新

--- towSum.js
+++ towSum.js
@@ -1,11 +1,22 @@
-const twoSum = (nums, target) => {
-  const hash = new Map();
-  for (let i = 0; i < nums.length; i++) {
-    // 判断是否存在目标值减去当前值的差的key 因为 target - x = y
-    if (hash.has(target - nums[i])) {
-      return [i, hash.get(target - nums[i])];
+// 两数之和
+// const twoSum = (nums, target) => {
+//   const hash = new Map();
+//   for (let i = 0; i < nums.length; i++) {
+//     // 判断是否存在目标值减去当前值的差的key 因为 target - x = y
+//     if (hash.has(target - nums[i])) {
+//       return [i, hash.get(target - nums[i])];
+//     }
+//     // 把当前数组的值做key，下标做值
+//     hash.set(nums[i], i);
+//   }
+// };
+
+const towSum = (nums, target) => {
+  const hash = {}
+  for (let i = 0;i < nums.length; i++) {
+    if (hash[target-nums[i]]){
+      return [i, hash.get(target - nums[i])]
     }
-    // 把当前数组的值做key，下标做值
-    hash.set(nums[i], i);
+    hash[target-nums[i]] = i
   }
-};
+}
